da2e33c4f4cd64e9f582db91aa92bb00c894af63
Loz Jackson <lozjackson@talktalk.net>
2016-05-21 12:21:02 +0100

    add index.js

---
+++ addon/index.js
@@ -0,0 +1,18 @@
+import sha512, {
+  hex_sha512,
+  b64_sha512,
+  any_sha512,
+  hex_hmac_sha512,
+  b64_hmac_sha512,
+  any_hmac_sha512
+} from 'ember-sha512/utils/sha512';
+
+export default sha512;
+export {
+  hex_sha512,
+  b64_sha512,
+  any_sha512,
+  hex_hmac_sha512,
+  b64_hmac_sha512,
+  any_hmac_sha512
+};
